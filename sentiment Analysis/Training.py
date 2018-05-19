69467f4ca08c4f2ba262601dd68a42a2d98505be
Shre1000 <31860419+shre1000@users.noreply.github.com>
2018-05-19 00:08:27 -0500

    Update Training.py

--- sentiment Analysis/Training.py
+++ sentiment Analysis/Training.py
@@ -1,4 +1,4 @@
-#Shreyasi Kokamthankar
+
 #This is training file
 #This is first file to execute.
 
@@ -36,16 +36,16 @@ class VoteClassifier(ClassifierI):
         conf = choice_votes / len(votes)
         return conf
     
-short_pos = open("/Users/kokam/Documents/files_or_661/positive.txt","r").read()
+short_pos = open("/Users/positive.txt","r").read()
 short_pos = short_pos.decode('utf-8')
-short_neg = open("/Users/kokam/Documents/files_or_661/negative.txt","r").read()
+short_neg = open("/Users/negative.txt","r").read()
 short_neg = short_neg.decode('utf-8')
 
 
 all_words = []
 documents = []
 
-# this is changed as per the suggessions given while presenting.
+
 #  j is adjective, r is adverb, and v is verb, n is noun
 allowed_word_types = ["J","R","V", "N"]
 #allowed_word_types = ["J"]
@@ -69,7 +69,7 @@ for p in short_neg.split('\n'):
 
 
 
-save_documents = open("/Users/kokam/Documents/files_or_661/pickled_algos/documents.pickle","wb")
+save_documents = open("/Users/documents.pickle","wb")
 pickle.dump(documents, save_documents)
 save_documents.close()
 
@@ -80,7 +80,7 @@ all_words = nltk.FreqDist(all_words)
 word_features = list(all_words.keys())[:5000]
 
 
-save_word_features = open("/Users/kokam/Documents/files_or_661/pickled_algos/word_features5k.pickle","wb")
+save_word_features = open("/Users/word_features5k.pickle","wb")
 pickle.dump(word_features, save_word_features)
 save_word_features.close()
 
@@ -95,7 +95,7 @@ def find_features(document):
 
 featuresets = [(find_features(rev), category) for (rev, category) in documents]
 
-save_featuresets = open("/Users/kokam/Documents/files_or_661/pickled_algos/featuresets.pickle","wb")
+save_featuresets = open("/Users/featuresets.pickle","wb")
 pickle.dump(featuresets, save_featuresets)
 save_featuresets.close()
 
@@ -112,7 +112,7 @@ print("Original Naive Bayes Algo accuracy percent:", (nltk.classify.accuracy(cla
 classifier.show_most_informative_features(15)
 
 
-save_classifier = open("/Users/kokam/Documents/files_or_661/pickled_algos/originalnaivebayes5k.pickle","wb")
+save_classifier = open("/Users/originalnaivebayes5k.pickle","wb")
 pickle.dump(classifier, save_classifier)
 save_classifier.close()
 
@@ -120,7 +120,7 @@ MNB_classifier = SklearnClassifier(MultinomialNB())
 MNB_classifier.train(training_set)
 print("MNB_classifier accuracy percent:", (nltk.classify.accuracy(MNB_classifier, testing_set))*100)
 
-save_classifier = open("/Users/kokam/Documents/files_or_661/pickled_algos/MNB_classifier5k.pickle","wb")
+save_classifier = open("/Users/MNB_classifier5k.pickle","wb")
 pickle.dump(MNB_classifier, save_classifier)
 save_classifier.close()
 
@@ -128,7 +128,7 @@ BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
 BernoulliNB_classifier.train(training_set)
 print("BernoulliNB_classifier accuracy percent:", (nltk.classify.accuracy(BernoulliNB_classifier, testing_set))*100)
 
-save_classifier = open("/Users/kokam/Documents/files_or_661/pickled_algos/BernoulliNB_classifier5k.pickle","wb")
+save_classifier = open("/Users/BernoulliNB_classifier5k.pickle","wb")
 pickle.dump(BernoulliNB_classifier, save_classifier)
 save_classifier.close()
 
@@ -136,7 +136,7 @@ LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
 LogisticRegression_classifier.train(training_set)
 print("LogisticRegression_classifier accuracy percent:", (nltk.classify.accuracy(LogisticRegression_classifier, testing_set))*100)
 
-save_classifier = open("/Users/kokam/Documents/files_or_661/pickled_algos/LogisticRegression_classifier5k.pickle","wb")
+save_classifier = open("/Users/LogisticRegression_classifier5k.pickle","wb")
 pickle.dump(LogisticRegression_classifier, save_classifier)
 save_classifier.close()
 
@@ -145,7 +145,7 @@ LinearSVC_classifier = SklearnClassifier(LinearSVC())
 LinearSVC_classifier.train(training_set)
 print("LinearSVC_classifier accuracy percent:", (nltk.classify.accuracy(LinearSVC_classifier, testing_set))*100)
 
-save_classifier = open("/Users/kokam/Documents/files_or_661/pickled_algos/LinearSVC_classifier5k.pickle","wb")
+save_classifier = open("/Users/LinearSVC_classifier5k.pickle","wb")
 pickle.dump(LinearSVC_classifier, save_classifier)
 save_classifier.close()
 
@@ -153,7 +153,7 @@ SGDC_classifier = SklearnClassifier(SGDClassifier())
 SGDC_classifier.train(training_set)
 print("SGDClassifier accuracy percent:",nltk.classify.accuracy(SGDC_classifier, testing_set)*100)
 
-save_classifier = open("/Users/kokam/Documents/files_or_661/pickled_algos/SGDC_classifier5k.pickle","wb")
+save_classifier = open("/Users/SGDC_classifier5k.pickle","wb")
 pickle.dump(SGDC_classifier, save_classifier)
 save_classifier.close()
 
